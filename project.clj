329f4a614714cfc0cb8acb676d0befb39932de9c
Santiago Gepigon <santiago.gepigon@gmail.com>
2017-09-21 21:35:30 -0400

    Add spec dependencies and commented code for development

--- project.clj
+++ project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[expound "0.3.0"]
+                 [org.clojure/clojure "1.9.0-beta1"]
+                 [org.clojure/core.specs.alpha "0.1.24"]
+                 [spec-provider "0.4.9"]]
   :main ^:skip-aot clj-data-science.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
